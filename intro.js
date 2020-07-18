871c3bcd3088dc7d482d47f347caf37782a6b985
shivani-chaurasia <68290755+shivani-chaurasia@users.noreply.github.com>
2020-07-18 14:30:42 +0530

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,51 @@
+console.log("heyyyy");
+
+//array
+
+let arr1 = ['I','GO','HOME'];
+console.log(arr1);
+console.log(Array.isArray(arr1));
+
+let arr3= 'sky';
+let arr2= 'blue';
+console.log(arr2.concat(arr3));
+
+let fruits= ['apple','banana','pear','cherry'];
+console.log(fruits.length);
+
+let NewLength= fruits.push('orange')
+console.log(NewLength);
+
+let last= fruits.pop()
+console.log(last);
+
+let pos= fruits.indexOf('banana');
+console.log(pos);
+
+const arr5=[1,2,3,4,5,6];
+arr5.includes(2);
+console.log(arr5);
+
+let arr4 = ['day1','day2','day3'];
+console.log(arr4[0]);
+console.log(arr4[1]);
+console.log(arr4[2]);
+
+const name="letsupgrade"
+const nameArray= Array.from(name);
+console.log(name);
+console.log(nameArray)
+
+//string
+let str="shivani";
+console.log(str);
+
+var txt="ABCDEFGHIJKL";
+var sln= txt.length;
+console.log(txt);
+
+var text= "excellant";
+text.slice[0,4];
+console.log(text.slice);
+
+
