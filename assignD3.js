ebc62fc415c10caf8bacac4130a2d77129480acb
shivani-chaurasia <68290755+shivani-chaurasia@users.noreply.github.com>
2020-07-20 12:49:02 +0530

    Add files via upload

---
+++ assignD3.js
@@ -0,0 +1,59 @@
+console.log("this is my day3 assignment") 
+
+//Q1
+
+/*let num = prompt("Enter number here")
+if (num % 2 ==0) {
+   console.log("The number entered is ``${num} and Number is EVEN!")   
+}
+else {
+   console.log("The number entered is ${num}and 34Number is ODD!")
+} */
+
+//Q2
+
+let str =prompt("Enter OS name   Version");
+let srt2= str.split[" "];
+console.log(str2);
+
+
+//Q3
+
+//if else if
+/* marks= 79;
+if (marks>=80) {
+    console.log("Your grade is A");
+}
+else if (marks>=50) {
+    console.log("Your grade is B");
+} 
+else {
+    console.log("Your grade is C");
+} */
+
+
+//ternary
+/*marks=78
+let grade= marks>=80? "Your grade is A": 
+marks>=50? "Your grade is B":
+"Your grade is C";
+console .log(grade);  */
+
+//switch
+/*let marks= 90;
+switch(true)
+    case 80:
+        if (grade= marks>=80)
+        console.log("Your grade is A");
+         break;
+    case 50:
+        if (grade= marks>=80)
+        console.log("Your grade is B");
+        break;
+    default :
+        console.log("Your grade is C");
+        break;
+}
+*/
+
+
